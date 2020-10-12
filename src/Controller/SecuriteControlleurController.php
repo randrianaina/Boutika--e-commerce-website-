ff3f5dc3ae37abe71409d5b7f30524a87de5702e
randrianaina <randrianaina@hotmail.com>
2020-10-12 16:56:38 +0200

    new changes 12/10/2020
    _ login form
    _ registration form

--- src/Controller/SecuriteControlleurController.php
+++ src/Controller/SecuriteControlleurController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Http\Util\TargetPathTrait;
 
 class SecuriteControlleurController extends AbstractController
 {
@@ -14,9 +15,9 @@ class SecuriteControlleurController extends AbstractController
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
+         if ($this->getUser()) {
+             return $this->redirectToRoute('acceuil');
+         }
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
